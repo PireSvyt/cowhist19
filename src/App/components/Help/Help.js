99f110088ecb23fbe87d5a6094bad7d98145b351
pst <81512700+PireSvyt@users.noreply.github.com>
2023-05-26 13:29:25 +0000

    Update 4 files

--- src/App/components/Help/Help.js
+++ src/App/components/Help/Help.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useSelector } from "react-redux";
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 
+// Resources
+import helps from "./resources/helps.json";
 // Components
-//import Documentation from "./components/Documentation/Documentation.js";
-//import Howto from "./components/Howto/Howto.js";
+import HelpSection from "./components/HelpSection.js";
 // Shared
 import Appbar from "../../shared/components/Appbar/Appbar.js";
-import ToComeModal from "../../shared/components/ToComeModal/ToComeModal.js";
-// Reducers
-import appStore from "../../store/appStore.js";
 
 export default function Help() {
   if (process.env.REACT_APP_DEBUG === "TRUE") {
@@ -19,32 +16,14 @@ export default function Help() {
   // i18n
   const { t } = useTranslation();
 
-  // Selects
-  const select = {
-    tocomeData: useSelector((state) => state.sliceToComeModal.tocomeData),
-  };
-
   return (
     <Box>
       <Appbar route="help" title={t("generic.label.help")} />
       <Box sx={{ height: 48 }} />
 
-      <Box textAlign="center">
-        <Box sx={{ height: 48 }} />
-        <Button
-          variant="outlined"
-          sx={{
-            width: "80%",
-            m: 1,
-          }}
-          onClick={() => {
-            appStore.dispatch({ type: "sliceToComeModal/open" });
-          }}
-        >
-          {t("generic.button.tocome")}
-        </Button>
-      </Box>
-      <ToComeModal data={select.tocomeData} />
+      {helps.map((help) => {
+        return <HelpSection section={help} depth={0} key={help.title} />;
+      })}
     </Box>
   );
 }

086c7f1a05947fb8e9113285c95b14ef04217204
Pire Svyt <savoyat.p@gmail.com>
2023-06-13 02:59:39 +0200

    existing pseudo

---
+++ src/App/shared/services/serviceExistingPseudo.js
@@ -0,0 +1,83 @@
+// Services
+import apiPseudo from "./apiPseudo.js";
+
+async function serviceExistingPseudo() {
+  if (process.env.REACT_APP_DEBUG === "TRUE") {
+    console.log("serviceExistingPseudo");
+  }
+
+  try {
+    // API call
+    const data = await apiPseudo();
+    if (process.env.REACT_APP_DEBUG === "TRUE") {
+      console.log("data.type : " + data.type);
+    }
+
+    // Response management
+    switch (data.type) {
+        case "auth.existingpseudo.true":
+            appStore.dispatch({
+                type: "sliceSignUpModal/change",
+                payload: {
+                    errors : {
+                        existingpseudo : true
+                    }
+                },
+            });
+            appStore.dispatch({
+                type: "sliceInviteModal/change",
+                payload: {
+                    errors : {
+                        existingpseudo : true
+                    }
+                },
+            });
+            break;
+        case "auth.existingpseudo.false":
+            appStore.dispatch({
+                type: "sliceSignUpModal/change",
+                payload: {
+                    errors : {
+                        existingpseudo : false
+                    }
+                },
+            });
+            appStore.dispatch({
+                type: "sliceInviteModal/change",
+                payload: {
+                    errors : {
+                        existingpseudo : false
+                    }
+                },
+            });
+            break;
+        case "auth.existingpseudo.error.onfind":
+            null
+            break;
+        default:
+            appStore.dispatch({
+                type: "sliceSnack/change",
+                payload: {
+                    uid: random_id(),
+                    id: "generic.snack.api.unmanagedtype",
+                    details: data.type,
+                },
+            });
+    }
+  } catch (err) {
+    if (process.env.REACT_APP_DEBUG === "TRUE") {
+      console.log("service caught error");
+      console.log(err);
+    }
+    // Error network
+    appStore.dispatch({
+      type: "sliceSnack/change",
+      payload: {
+        uid: random_id(),
+        id: "generic.snack.api.errornetwork",
+      },
+    });
+  }
+}
+
+export default serviceExistingPseudo;

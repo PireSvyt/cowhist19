12c16d97ecf973542dde8e7282ed39bcfa052023
Pire Svyt <savoyat.p@gmail.com>
2023-06-13 03:38:46 +0200

    fix

--- src/App/shared/services/apiPseudo.js
+++ src/App/shared/services/apiPseudo.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 async function apiPseudo(pseudoInput) {
-  console.log("pseudoInput")
-  console.log(pseudoInput)
   try {
     const res = await axios.post(
       process.env.REACT_APP_SERVER_URL + "/auth/v1/existingpseudo",

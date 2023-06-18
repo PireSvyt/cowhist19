926ec26e89e174f0027e9a30dda5ce905dd097fb
Pire Svyt <savoyat.p@gmail.com>
2023-06-18 23:15:57 +0200

    ga

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import ReactGA from "react-ga4";
+//import ReactGA from "react-ga4";
 
 import App from "./App/App.js";
 
 import "./i18n/i18n-config.js";
 import appStore from "./App/store/appStore.js";
 
-ReactGA.initialize([{ trackingId: process.env.GA_MEASUREMENT_ID }])
+//ReactGA.initialize([{ trackingId: process.env.GA_MEASUREMENT_ID }])
 
 const root = createRoot(document.getElementById("root"));
 root.render(

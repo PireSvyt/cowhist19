f9215b8a94a0c9c6614dbfaffbe1960de4cf20bd
Pire Svyt <savoyat.p@gmail.com>
2023-06-21 08:36:57 +0200

    changes

--- src/App/components/Table/components/TableStats/components/StatGraph/StatGraph.js
+++ src/App/components/Table/components/TableStats/components/StatGraph/StatGraph.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 
 export default function StatGraph(props) {
     if (process.env.REACT_APP_DEBUG === "TRUE") {
       console.log("StatGraph");
     }
+    // i18n
+    const { t } = useTranslation();
+  
+    // Selects
+    const select = {
+      loadedDetails: useSelector((state) => state.sliceTableDetails.loaded),
+      loadedStats: useSelector((state) => state.sliceTableStats.loaded),
+      stats: useSelector((state) => state.sliceTableStats.stats),
+      players: useSelector((state) => state.sliceTableDetails.players),
+      view: useSelector((state) => state.sliceTableStats.view),
+    };
+
+
     return (
     <Plot
     data={[
         {
-        x: [1, 2, 3],
-        y: [2, 6, 3],
-        type: 'scatter',
-        mode: 'lines',
-        marker: {color: 'red'},
+          x: [1, 2, 3], y: [2, 6, 3],
+          type: 'scatter',
+          mode: 'lines',
+          marker: {color: 'red'},
         },
-        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        {
+          type: 'bar', x: [1, 2, 3], y: [2, 5, 3]
+        },
     ]}
     layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
     />

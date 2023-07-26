29065f016beefc8025bbd1b8cb10b6d732c78d05
Pire Svyt <savoyat.p@gmail.com>
2023-07-26 07:34:34 +0200

    restruct

---
+++ src/App/Core/Table/apiTableDetails.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// Reducers
+import appStore from "../../store/appStore.js";
+
+async function apiTableDetails(id) {
+  try {
+    const res = await axios.get(
+      process.env.REACT_APP_SERVER_URL + "/table/v2/" + id,
+      {
+        headers: {
+          Authorization: "Bearer " + appStore.getState().sliceUserAuth.token,
+        },
+      }
+    );
+    return res.data;
+  } catch (err) {
+    return err.response.data;
+  }
+}
+
+export default apiTableDetails;

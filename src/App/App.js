b71f60d0e84480a31168211be049b6228a0bd8f4
Pire Svyt <savoyat.p@gmail.com>
2023-07-31 08:07:29 +0200

    come back prior refacto NOT FUNCTIONAL

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Components
 import Home from "./components/Home/Home.js";
 import Activation from "./components/Activation/Activation.js";
+import PasswordReset from "./components/PasswordReset/PasswordReset.js";
 import Table from "./components/Table/Table.js";
 import Account from "./components/Account/Account.js";
 import Help from "./components/Help/Help.js";
@@ -24,8 +25,9 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/activation/:id" element={<Activation />} />
+        <Route exact path="/" element={<Home />} />        
+        <Route path="/activation/:token" element={<Activation />} />
+        <Route path="/passwordreset/:token" element={<PasswordReset />} />
         <Route path="/account" element={<Account />} />
         <Route path="/table/:id" element={<Table />} />
         <Route path="/help" element={<Help />} />
